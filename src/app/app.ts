2bfce80424bac0ca6d53756741529aefba2200c4
nota2k <nelly.babillon@gmail.com>
2026-02-17 14:33:53 +0100

    Update index title, add Home component, and implement housing location structure
    - Changed the title in index.html from "LearnAngular" to "Home".
    - Introduced Home component with its HTML, CSS, and test files.
    - Created HousingLocation interface and component with associated HTML, CSS, and test files.
    - Updated app component to include the Home component in imports.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { Home} from './home/home';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Home, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
