a40cea2c60e7ce4a21501a9582bdc670aa4da123
nota2k <nelly.babillon@gmail.com>
2026-02-17 15:28:18 +0100

    Refactor housing location structure and update Home component
    - Renamed HousingLocation interface to HousingLocationInfo for clarity.
    - Updated Home component to display a list of housing locations using *ngFor.
    - Enhanced HousingLocation component to accept housingLocation as an input and display relevant details.
    - Added sample housing location data in Home component for demonstration.

--- src/app/housing-location/housing-location.spec.ts
+++ src/app/housing-location/housing-location.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HousingLocation } from './housing-location';
+import { HousingLocationComponent } from './housing-location';
 
 describe('HousingLocation', () => {
-  let component: HousingLocation;
-  let fixture: ComponentFixture<HousingLocation>;
+  let component: HousingLocationComponent;
+  let fixture: ComponentFixture<HousingLocationComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HousingLocation]
+      imports: [HousingLocationComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HousingLocation);
+    fixture = TestBed.createComponent(HousingLocationComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocationInfo } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
@@ -6,6 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './housing-location.html',
   styleUrl: './housing-location.css',
 })
-export class HousingLocation {
-
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocationInfo;
 }
